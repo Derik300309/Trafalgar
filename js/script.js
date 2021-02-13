4d6ff7e9f98d935be46f6d34707d44605f37489a
Derik300309 <derik3009@gmail.com>
2021-02-13 11:44:28 +0500

    The final version

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 100,
+    grabCursor: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
